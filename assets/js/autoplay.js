ec4dbd875bf165f1c55c7f5d53acc87e6a5f1994
shabari <shabarisvg13@gmail.com>
2024-01-06 21:03:51 +0530

    Fixed Youtube Size Issue

--- assets/js/autoplay.js
+++ assets/js/autoplay.js
@@ -1,41 +1,29 @@
 function playVideo() {
-    // Get the video container element
-    var videoContainer = document.getElementById('video-container');
+  var videoContainer = document.getElementById("video-container");
+  videoContainer.style.display = "block";
 
-    // Show the video container
-    videoContainer.style.display = 'block';
+  var closeButton = document.getElementById("close-button");
+  closeButton.style.display = "block";
 
-    // Create an iframe element for the YouTube video
-    var iframe = document.createElement('iframe');
-    iframe.src ='https://www.youtube.com/embed/pjsbX16n1y0?si=hKvCl52E1J5jb74N&autoplay=1';
-     iframe.width = '560';
-     iframe.height = '315';
-    iframe.frameBorder = '0';
-    iframe.allow = 'autoplay; encrypted-media';
-    videoContainer.appendChild(iframe);
+  var iframe = document.createElement("iframe");
+  iframe.src =
+    "https://www.youtube.com/embed/pjsbX16n1y0?si=hKvCl52E1J5jb74N&autoplay=1";
+  iframe.frameBorder = "0";
+  iframe.width="560";
+  iframe.height="315";
+  iframe.allow = "autoplay; encrypted-media";
+  videoContainer.appendChild(iframe);
 
-    // Hide the play button after clicking
-    document.getElementById('play-button').style.display = 'none';
-
-    // // Show the close button
-    var closeButton = document.getElementById('close-button');
-    closeButton.style.display = 'block';
-  
+  document.getElementById("play-button").style.display = "none";
 }
-  function closeVideo() {
-    // Get the video container and close button elements
-    var videoContainer = document.getElementById('video-container');
-    var closeButton = document.getElementById('close-button');
 
-    // Remove the iframe
-    videoContainer.innerHTML = '';
+function closeVideo() {
+  var videoContainer = document.getElementById("video-container");
+  var closeButton = document.getElementById("close-button");
 
-    // Hide the video container
-    videoContainer.style.display = 'none';
+  videoContainer.innerHTML = "";
+  videoContainer.style.display = "none";
 
-    // Show the play button
-    document.getElementById('play-button').style.display = 'block';
-
-    
-    closeButton.style.display = 'none';
-  }
+  document.getElementById("play-button").style.display = "block";
+  closeButton.style.display = "none";
+}
